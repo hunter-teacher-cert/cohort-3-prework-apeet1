3c5aaa82243bdcb2602700557c37a985422d8a19
apeet1 <97042554+apeet1@users.noreply.github.com>
2022-01-13 19:09:08 +0000

    Created the file Time.java

--- pre02/Time.java
+++ pre02/Time.java
@@ -1,19 +1,45 @@
-public class Date {
+public class Time {
   public static void main(String[] args) {
-    String firstLine = "Hello, again!";
-    System.out.println(firstLine);
 
-    System.out.print("The value of FirstLine is ");
-    System.out.println(firstLine);
-
-    int hour = 11;
-    int minute = 59;
-    System.out.print("The current time is ");
-    System.out.print(hour);
-    System.out.print(":");
-    System.out.print(minute);
-    System.out.println(".");
+    int startHour = 9;
+    int startMinute = 35;
+    int startSecond = 23;
     
+    System.out.print("My start time was ");
+    System.out.print(startHour);
+    System.out.print(":");
+    System.out.print(startMinute);
+    System.out.print(" am");
+    System.out.println(".");
+
+    System.out.print("The number of seconds since midnight was ");
+    System.out.print(startHour * 3600 + startMinute * 60 + startSecond);
+    System.out.println(" seconds.");
+
+    int secondsInDay = 86400;
+    System.out.print("The number of seconds remaining in the day was ");
+    System.out.print(secondsInDay - (startHour * 3600 + startMinute * 60 + startSecond) );
+    System.out.println(" seconds.");
+
+    int endHour = 10;
+    int endMinute = 49;
+    int endSecond = 50;
+
+    System.out.print("The current time is ");
+    System.out.print(endHour);
+    System.out.print(":");
+    System.out.print(endMinute);
+    System.out.print(" am");
+    System.out.println(".");
+
+    System.out.print("The amount of time I spent on this project was ");
+    System.out.print(endHour - startHour);
+    System.out.print(" hour, ");
+    System.out.print(endMinute - startMinute);
+    System.out.print(" minutes, and ");
+    System.out.print(endSecond - startSecond);
+    System.out.println(" seconds.");
+
   }
 
 }

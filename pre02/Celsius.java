0f6b22347e0568b73d2c090a850217aa93f109d0
apeet1 <97042554+apeet1@users.noreply.github.com>
2022-01-14 15:34:07 +0000

    Updated Celsius.java and Guess.java

--- pre02/Celsius.java
+++ pre02/Celsius.java
@@ -1,22 +1,18 @@
 import java.util.Scanner;
 
-public class Celsius {
-  public static void main(String[] args) {
-
-int Celsius;
-double Fahrenheit;
-Scanner in = new Scanner(System.in);
-
-System.out.println("How many degrees Celsius? ");
-
-Celsius = in.nextInt();
-Fahrenheit = Celsius * 9.0 / 5.0 + 32;
-System.out.print(Celsius + " degrees Celsius = ");
-System.out.println(Fahrenheit + " degrees Fahrenheit");
-
-//I'm wondering if you can use a degrees symbol
-
-  }
-}  
-
+public class Celsius { 
+  
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
     
+    // prompt user for temperture input
+    System.out.println("Enter a temperature in degrees Celsius: ");
+    double Cels;
+    Cels = in.nextDouble();
+    
+    // convert C to F and output
+    double Fahr = Cels * 9/5 + 32; 
+    System.out.printf("%.1f C = %.1f F\n", Cels, Fahr);
+  }
+}
+ 
--- pre02/Guess.java
+++ pre02/Guess.java
@@ -1,30 +1,20 @@
+import java.util.Scanner;
 import java.util.Random;
 
-<<<<<<< HEAD
 public class Guess {
   public static void main(String[] args) {
-    //pick a random number
-    
+    Scanner in = new Scanner(System.in);
     Random random = new Random();
-    int number = random.nextInt(100) + 1;
-    System.out.println(number);
+    int number = random.nextInt(100) + 1; 
 
     System.out.println("I'm thinking of a number between 1 and 100 (including both.) Can you guess what it is?");
-    System.out.println("Type a number: ");
-    line = in.nextLine();
-    System.out.println("Your guess is: " + line);
+    System.out.print("Type a number: "); 
+    
+    int guess = in.nextInt();
+    int error = Math.abs(number - guess);
+    System.out.println("Your guess is: " + guess);
+    System.out.println("The number I was thinking of is: " + number);
+    System.out.println("You were off by: " + error);
 
   }
 }
-=======
-public class GuessStarter {
-    public static void main(String[] args) {
-      //pick a random number
-      Random = new Random();
-      int number = random.nextInt(100) + 1
-      System.out.println(number)
-
-      
-    }
-}
->>>>>>> origin/master

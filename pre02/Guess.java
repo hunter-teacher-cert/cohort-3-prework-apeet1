b5ee2f6f5c7489cb014e420bed58cc123c1ca1c1
apeet1 <97042554+apeet1@users.noreply.github.com>
2022-01-14 02:09:28 +0000

    Added file Guess.java

---
+++ pre02/Guess.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class GuessStarter {
+    public static void main(String[] args) {
+      //pick a random number
+      Random = new Random();
+      int number = random.nextInt(100) + 1
+      System.out.println(number)
+
+      
+    }
+}
